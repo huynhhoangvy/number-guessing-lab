3a26fd49c7f55f559763def6b4e7f0c781bb6fda
huynhhoangvy <huynh.h.vy@gmail.com>
2019-05-15 21:10:34 +0700

    add and edit buttons

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ let numberOfAvailableGuesses = 10
 
 function guessNumber() {
   if (numberOfAvailableGuesses  === 0 ) {
-    return
+    newGame();
+    return;
   }
-  const generatedRandomNumber = '10'
+  const generatedRandomNumber = Math.floor(Math.random() *100);
+  console.log(generatedRandomNumber)
   const guess = document.getElementById('guess').value
 
   if (guess < generatedRandomNumber) {
@@ -17,14 +19,44 @@ function guessNumber() {
 
   if (guess === generatedRandomNumber) {
     document.getElementById('correctGuess').style.visibility = 'visible'
+    newGame();
+    return;
+  }
+  document.getElementById("guess-btn").style.visibility = "hidden"
+  document.getElementById("guessAgain-btn").style.visibility = "visible"
+  document.getElementById('guess').value = ''
+  const remainingGuesses = document.getElementById('numberOfRemainingGuesses').innerHTML
+  document.getElementById('numberOfRemainingGuesses').innerHTML = parseInt(remainingGuesses) - 1
+  numberOfAvailableGuesses -= 1
+  console.log(numberOfAvailableGuesses)
+}
+
+function newGame() {
+  numberOfAvailableGuesses = 10
+  document.getElementById('numberOfRemainingGuesses').innerHTML = 10;
+  document.getElementById("guess-btn").style.visibility = "visible"
+  document.getElementById('wrongGuess').style.visibility = 'hidden'
+  document.getElementById('correctGuess').style.visibility = 'hidden'
+  generatedRandomNumber = 0;
+
+
+}
+
+function guessAgain() {
+  if (numberOfAvailableGuesses === 0 ) {
+
   }
   document.getElementById('guess').value = ''
   const remainingGuesses = document.getElementById('numberOfRemainingGuesses').innerHTML
   document.getElementById('numberOfRemainingGuesses').innerHTML = parseInt(remainingGuesses) - 1
   numberOfAvailableGuesses -= 1
+  console.log(numberOfAvailableGuesses)
 }
 
-function resetGame() {
-  numberOfAvailableGuesses = 10
-  document.getElementById('numberOfRemainingGuesses').innerHTML = 10
+function addElementToGuessLogArray () {
+  let guessHistoryArray = [];
+  for (var i = 0; i < 10; I ++) {
+    numberOfAvailableGuesses -= 1;
+    guessHistoryArray.push(document.getElementById('guess').value = '')
+  }
 }

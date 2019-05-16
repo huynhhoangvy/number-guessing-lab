3764001c472f156c7e8136b5cea9670304ee0ea8
huynhhoangvy <huynh.h.vy@gmail.com>
2019-05-16 17:56:22 +0700

    goin home, update work from class, timer got broken when press guess again

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ let numberOfAvailableGuesses = DEFAULT_GUESSES
 document.getElementById("numberOfRemainingGuesses").innerHTML = numberOfAvailableGuesses;
 let generatedRandomNumber, lastGuessValue
 let guessHistory = []
+let previousRoundsGuessHistory = []
+let number = 0
+let bestResult = []
+let second = 0;
 
 newGame()
 
@@ -10,60 +14,75 @@ function newGame() {
   numberOfAvailableGuesses = DEFAULT_GUESSES
   generatedRandomNumber = Math.floor(Math.random() * 100);
   console.log("random " + generatedRandomNumber)
-  document.getElementById('numberOfRemainingGuesses').innerHTML = numberOfAvailableGuesses;
+  document.getElementById("numberOfRemainingGuesses").innerHTML = numberOfAvailableGuesses;
   document.getElementById("guess-btn").disabled = false
   document.getElementById("guess").disabled = false
   document.getElementById("guess-btn").style.visibility = "visible"
-  document.getElementById('guessTooSmallError').style.visibility = 'hidden'
-  document.getElementById('guessTooBigError').style.visibility = 'hidden'
-  document.getElementById('correctGuess').style.visibility = 'hidden'
-  document.getElementById("btn_new_game").style.visibility = "hidden"
+  document.getElementById("guessTooSmallError").style.visibility = "hidden"
+  document.getElementById("guessTooBigError").style.visibility = "hidden"
+  document.getElementById("correctGuess").style.visibility = "hidden"
+  document.getElementById("btn_new_game").style.visibility = "visible"
   document.getElementById("game-over-div").style.visibility = "hidden"
+  document.getElementById("guess").value = ""
+  document.getElementById("bestResult").innerHTML = bestResult
+
   guessHistory = []
-  document.getElementById('guess').value = ''
+  document.getElementById("guessHistory").innerHTML = guessHistory
+  number += 1
+  document.getElementById("number").innerHTML = number
+  countDown()
 }
 
 function checkGuess() {
   let guessValue = document.getElementById("guess").value
-
-  // if (guessValue === lastGuessValue) {
-  //   alert()
-  // }
   console.log("guessvalue " + guessValue.toString() + " randomnumber " + generatedRandomNumber)
-  // for (var i = 0; i < guessHistory.length; i ++) {
-  //   if (guessHistory[i] === guessValue) {
-  //     alert ("You did it before smartass!")
-  //   }
-  // }
-  // array.indexof kiem tra phan tu da co trong array hay chua, xet indexof < 0 ====== phan tu moi
-  // if(guessHistory.indexOf(guessValue) < 0) {
-  //   alert("You did guess the same before!")
-  // }
-  // array.includes ???
  
-  if (guessHistory.includes(guessValue)) {
-    alert("Pick another number")
-    return;
-  }
+if (guessHistory.includes(guessValue)) {
+  alert("Pick another number")
+  return;
+}
+countDown()
+
+
   guessHistory.push(guessValue);
-  document.getElementById('array').innerHTML = guessHistory;
+  document.getElementById("guessHistory").innerHTML = guessHistory;
+  previousRoundsGuessHistory.push(guessValue)
+  document.getElementById("previousRoundsGuessHistory").innerHTML = previousRoundsGuessHistory
+  
   if (numberOfAvailableGuesses === 1 && guessValue !== generatedRandomNumber) {
     updateUiGameOver()
   } else {
     if (guessValue < generatedRandomNumber) {
-      document.getElementById('guessTooSmallError').style.visibility = 'visible'
+      document.getElementById("guessTooSmallError").style.visibility = "visible"
       updateUiForEachTurn();
     } else if (guessValue > generatedRandomNumber) {
-      document.getElementById('guessTooBigError').style.visibility = 'visible'
+      document.getElementById("guessTooBigError").style.visibility = "visible"
       updateUiForEachTurn();
     } else {
+      updateBestResult()
       updateUiWinner()
     }
   }
 }
 
+function countDown() {
+  second = 0
+  setInterval(incrementSections, 1000)
+}
+
+function incrementSections() {
+  second += 1
+  document.getElementById("elapsedTime").innerHTML = "Elapsed time: " + second + " seconds."
+}
+
+function updateBestResult() {
+  bestResult.push(DEFAULT_GUESSES - numberOfAvailableGuesses + 1)
+  // bestResult.push(guessValue)
+  document.getElementById("bestResult").innerHTML = bestResult
+}
+
 function updateUiGameOver() {
-  document.getElementById('numberOfRemainingGuesses').innerHTML = 0
+  document.getElementById("numberOfRemainingGuesses").innerHTML = 0
   document.getElementById("game-over-div").style.visibility = "visible"
   document.getElementById("guess-btn").disabled = true
   document.getElementById("guess").disabled = true
@@ -73,16 +92,12 @@ function updateUiGameOver() {
 
 function updateUiWinner() {
   document.getElementById("btn_new_game").style.visibility = "visible"
-  document.getElementById('correctGuess').style.visibility = 'visible'
+  document.getElementById("correctGuess").style.visibility = "visible"
   document.getElementById("guess").disabled = true
   document.getElementById("guess-btn").disabled = true
 }
 
 function updateUiForEachTurn() {
-  document.getElementById('guess').value = ''
-  document.getElementById('numberOfRemainingGuesses').innerHTML = --numberOfAvailableGuesses
+  document.getElementById("guess").value = ""
+  document.getElementById("numberOfRemainingGuesses").innerHTML = --numberOfAvailableGuesses
 }
-
-// function updateUiDuplicateGuess() {
-// return guessValue
-// }
